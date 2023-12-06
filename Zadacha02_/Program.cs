4b1ba490fe4b7cc6275ab9be7a4b4c194bc1ca02
Evgeny Korostelev <ddc_smoke@mail.ru>
2023-12-06 11:05:47 +0300

    add and reshil zadachu 2 v Zadacha02_

---
+++ Zadacha02_/Program.cs
@@ -0,0 +1,6 @@
+﻿// Задайте строку, содержащую латинские буквы в обоих регистрах. Сформируйте строку,
+// в которой все заглавные буквы заменены на строчные.
+Console.Write("Введите строку: ");
+string beforeString = Console.ReadLine()!;
+string afterString  = beforeString.ToLower();
+Console.WriteLine($"Измененная строка: {afterString}");
